7867c7f96bb66e985ff9e6c4d61d3ec1efa3a0e9
felixzhao <felixz2010@gmail.com>
2013-07-03 16:32:45 +0800

    init Rule Generater solution

---
+++ RuleGen/ICTCLAS.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ICTCLASLib
+{
+    enum eWordRootType
+    {
+        NATURE_N , // n. 
+        NATURE_V,  // v. 
+        NATURE_ADJ, // adj
+        NATURE_ADV, // adv
+        NATURE_PRON, // pron
+        NATURE_PREP, //prep
+        NATURE_CONJ, // conj
+        NATURE_T,    // time
+        NATURE_W,    // punctuation
+        NATURE_F,     // 方位词
+        NATURE_M,	//	数词 数语素
+        NATURE_Q 	//	量词 量语素
+    }
+    class SegWord
+    {
+        public string szWord;
+        public string wordType; 
+        public int Number; 
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+    // character coding types 
+    //////////////////////////////////////////////////////////////////////////
+    enum eCodeType
+    {
+        CODE_TYPE_UNKNOWN,//type unknown
+        CODE_TYPE_ASCII,//ASCII
+        CODE_TYPE_GB,//GB2312,GBK,GB10380
+        CODE_TYPE_UTF8,//UTF-8
+        CODE_TYPE_BIG5//BIG5
+    }
+
+
+    [StructLayout(LayoutKind.Sequential, Pack =1)]
+    public struct result_t
+    {
+        [MarshalAs(UnmanagedType.U4)]
+        public int start;
+        [MarshalAs(UnmanagedType.U4)]
+        public int length;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst =4)]
+        public byte[] szPos;
+        [MarshalAs(UnmanagedType.U4)]
+        public int sPosLow;
+        [MarshalAs(UnmanagedType.U4)]
+        public int POS_id;
+        [MarshalAs(UnmanagedType.U4)]
+        public int word_ID;
+        [MarshalAs(UnmanagedType.U4)]
+        public int word_type;
+        [MarshalAs(UnmanagedType.U4)]
+        public int weight;
+
+    }
+
+    class ICTCLAS
+    {
+        public static bool initialize()
+        {
+            if (!ICTCLAS_Init(null))
+                return false;
+            ICTCLAS_ImportUserDict("userdic.txt", eCodeType.CODE_TYPE_UNKNOWN);
+            return true; 
+        }
+
+        public static void splitword(string src, ArrayList wordList )
+        {
+            char[] chars = src.ToCharArray();
+            result_t[] result = new result_t[src.Length];
+            int i = 0;
+
+            int nWrdCnt = ICTCLAS_ParagraphProcessAW(src, result, eCodeType.CODE_TYPE_UNKNOWN, 1 );
+            result_t r;
+            //取字符串真实长度:
+            byte[] mybyte = System.Text.Encoding.Default.GetBytes(src);
+            byte[] byteWord;
+            for (i = 0; i < nWrdCnt; i++)
+            {
+                r = result[i];
+                //String sWhichDic = "";
+
+                //switch (r.word_type)
+                //{
+                //    case 0:
+                //        sWhichDic = "核心词典";
+                //        break;
+                //    case 1:
+                //        sWhichDic = "用户词典";
+                //        break;
+                //    case 2:
+                //        sWhichDic = "专业词典";
+                //        break;
+                //    default:
+                //        break;
+                //}
+
+                byteWord = new byte[r.length];
+                //取字符串一部分
+                Array.Copy(mybyte, r.start, byteWord, 0, r.length);
+                SegWord segWord = new SegWord();
+                segWord.szWord = System.Text.Encoding.Default.GetString(byteWord);
+                segWord.wordType = System.Text.Encoding.Default.GetString(r.szPos).Trim('\0');
+                segWord.Number = i ; 
+                wordList.Add(segWord);
+                //Console.WriteLine("No.{0}:start:{1}, length:{2},word_type:{3},Word_ID:{4}, UserDefine:{5}, Word:{6}\n", i, r.start, r.length, wordType, r.word_ID, sWhichDic, wrd);
+            }
+
+        }
+
+        public static void uninitialize()
+        {
+             ICTCLAS_Exit();
+        }
+
+            
+        #region DLL
+        [DllImport("ICTCLAS50.dll", CharSet = CharSet.Ansi, EntryPoint = "ICTCLAS_Init",CallingConvention = CallingConvention.Cdecl)]
+        private static extern bool ICTCLAS_Init(String sInitDirPath);
+
+        [DllImport("ICTCLAS50.dll", CharSet = CharSet.Ansi, EntryPoint = "ICTCLAS_Exit", CallingConvention = CallingConvention.Cdecl)]
+        private static extern bool ICTCLAS_Exit();
+
+        [DllImport("ICTCLAS50.dll", CharSet = CharSet.Ansi, EntryPoint = "ICTCLAS_ParagraphProcessAW", CallingConvention = CallingConvention.Cdecl)]
+        private static extern int ICTCLAS_ParagraphProcessAW(String sParagraph, [Out, MarshalAs(UnmanagedType.LPArray)]result_t[] result, eCodeType eCT, int bPOSTagged );
+
+        [DllImport("ICTCLAS50.dll", CharSet = CharSet.Ansi, EntryPoint = "ICTCLAS_ImportUserDict", CallingConvention = CallingConvention.Cdecl)]
+        private static extern int ICTCLAS_ImportUserDict(String sFilename, eCodeType eCT);
+
+        [DllImport("ICTCLAS50.dll", CharSet = CharSet.Ansi, EntryPoint = "ICTCLAS_FileProcess", CallingConvention = CallingConvention.Cdecl)]
+        private static extern double ICTCLAS_FileProcess(String sSrcFilename, eCodeType eCt, String sDsnFilename, int bPOStagged);
+        #endregion
+    }
+}
---
+++ RuleGen/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.Xml;
+using ICTCLASLib;
+namespace RuleGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] argsList = Environment.GetCommandLineArgs();
+            if (argsList.Length != 2)
+            {
+                Console.WriteLine("Usage: RuleGen <file name> or  RuleGen <directory name> ");
+                return;
+            }
+            string[] fileList ;
+            if (Directory.Exists(argsList[1]))
+            {
+                DirectoryInfo di = new DirectoryInfo(argsList[1]);
+                fileList = Directory.GetFiles(argsList[1], "*.txt");
+            }
+            else if (File.Exists(argsList[1]))
+            {
+                fileList = new string[1];
+                fileList[0] = argsList[1];
+            }
+            else{
+                Console.WriteLine("Invalid file or directory name ");
+                return;
+            }
+            if (!ICTCLAS.initialize())
+            {
+                Console.WriteLine("Initialize ICTCLAS libary failed!");
+                return;
+            }
+            foreach (string fileName in fileList)
+            {
+                StreamReader rd = new StreamReader(fileName, Encoding.Default);
+                string filedata = rd.ReadToEnd().Trim().Replace("\r\n", "");
+                char[] delimiterChars = { '。', '！', '?' };
+                string[] seArray = filedata.Split(delimiterChars);
+
+                XmlTextWriter xmlWriter = null;
+                int i = 1;
+                int wordOffset = 1;
+                foreach (string s in seArray)
+                {
+
+                    if (s != string.Empty)
+                    {
+                        ArrayList wordList = new ArrayList();
+                        ICTCLAS.splitword(s, wordList);
+                        if (xmlWriter == null)
+                            xmlWriter = beginWriteXML(fileName);
+                        writeWordsToXML(xmlWriter, wordList, i, ref wordOffset);
+
+                    }
+                    i++;
+                }
+                endWriteXML(xmlWriter);
+            }
+            ICTCLAS.uninitialize();
+        }
+        static XmlTextWriter beginWriteXML(string srcFileName)
+        {
+            FileInfo fileInfo = new FileInfo(srcFileName);
+            string name = fileInfo.FullName.TrimEnd(fileInfo.Extension.ToCharArray());
+            name += ".xml";
+            XmlTextWriter xmlWriter = new XmlTextWriter(name, Encoding.Default);
+            xmlWriter.Formatting = Formatting.Indented;
+            xmlWriter.WriteStartDocument();
+            xmlWriter.WriteStartElement("document");
+            xmlWriter.WriteAttributeString("name", fileInfo.Name);
+            return xmlWriter;
+        }
+        static void writeWordsToXML(XmlTextWriter wr, ArrayList wordList, int sentenceId, ref int wordOffset)
+        {
+            if (wr == null)
+                return;
+            if (wordList.Count == 0)
+                return;
+            wr.WriteStartElement("sentence");
+            wr.WriteAttributeString("id", sentenceId.ToString());
+            wr.WriteAttributeString("emotion", "N/A");
+            foreach (SegWord word in wordList)
+            {
+                wr.WriteStartElement("word");
+                wr.WriteAttributeString("id", wordOffset.ToString());
+                int order = word.Number + 1;
+                wr.WriteAttributeString("wordOrder", order.ToString());
+                wr.WriteAttributeString("emotion", "N/A");
+                if (word.wordType == "c" || word.wordType == "cc")
+                {
+                    wr.WriteAttributeString("conjunction", true.ToString());
+                    wr.WriteAttributeString("effect", "same");
+                }
+                wr.WriteAttributeString("property", word.wordType);
+                wr.WriteString(word.szWord);
+                wr.WriteEndElement();
+                wordOffset++;
+            }
+            wr.WriteEndElement();
+        }
+        static void endWriteXML(XmlTextWriter wr)
+        {
+            if (wr == null)
+                return;
+            wr.WriteEndElement();
+            wr.WriteEndDocument();
+            wr.Close();
+        }
+
+    }
+
+}
