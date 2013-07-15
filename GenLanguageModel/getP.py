f5b25003b0ad0610d69ed4f1c813711e39d7effc
felixzhao <felixz2010@gmail.com>
2013-07-15 17:16:55 +0800

    n-gram of term gen code, done

--- GenLanguageModel/getP.py
+++ GenLanguageModel/getP.py
@@ -14,5 +14,22 @@ def getP(doclist, term):
 	p = 1
 	input = term.split(' ')
 	for i in range(len(input)):
-		p *= getsubP(doclist,input[i:])
+		feq = getfeq(doclist, input[i:])
+		per = getfeq(doclist, input[i:-1])
+		p *= feq/float(per)
 	return p
+
+def getngrams( term, n ):
+    ngrams = []
+	for i in xrange( len(term) ):
+		if i == 0:
+			ngrams.append(term[-n:])
+		else:
+		ngrams.append( term[-n-i:-i])
+    return ngrams
+	
+def main():
+
+	
+if __name__ == "__main__":
+    main()
